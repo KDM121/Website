7bae5f0dce33df0d797d0d68fe4be46d528b337e
KDM121 <krnmcdnnll@gmail.com>
2023-03-01 19:21:41 +0000

    Fix: favicon

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Kierans Website</title>
         <meta name="description" content="Kieran's Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/Favicon.ico" />
       </Head>
       <main className="bg-background h-full w-full">
         <div>
