2b30a61e51a7325bc24cba657ec2e71ad266217d
KDM121 <krnmcdnnll@gmail.com>
2023-03-10 21:25:59 +0000

    fix fixed stuff

--- pages/projects.js
+++ pages/projects.js
@@ -37,7 +37,54 @@ export default function Home() {
           <div className="drawer drawer-end">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
 
-            <div className="drawer-content"> {/* Main*/}</div>
+            <div className="drawer-content">
+              {" "}
+              {/* Main*/}
+              <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
+                <div className="carousel-item">
+                  <img
+                    src="/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="/images/stock/photo-1494253109108-2e30c049369b.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="/images/stock/photo-1559181567-c3190ca9959b.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+              </div>
+            </div>
             <div className="drawer-side">
               <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
               <ul className="menu p-4 w-80 bg-neutral/60 text-base-content border-x-info border-x-8 bg-gradient-to-b from-primary/60 to-secondary/60">
